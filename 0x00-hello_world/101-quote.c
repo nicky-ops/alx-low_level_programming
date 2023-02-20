89afac0e14fb87edec033322558d6377c3f8c345
Nickson K Rop <nickyrutto@gmail.com>
2023-02-19 21:28:12 -0800

    Fixed return value of main

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,11 @@
 #include <unistd.h>
 
 /**
+ * main - Entry point
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error."
- * Returns: always 1
+ * Returns: Always (1)
+ *
  */
 int main(void)
 {
