99382a4aee26c6dd87487db0a18a03be4b59a7de
Nickson K Rop <nickyrutto@gmail.com>
2023-02-19 18:53:05 -0800

    Pritn out the size of various types in C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#inlcude <stdio.h>
+
+/**
+ * main - entry point
+ * Return : always 0
+ * int main(void)- does not take any input parameters.
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
