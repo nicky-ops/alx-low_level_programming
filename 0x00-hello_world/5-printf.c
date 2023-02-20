9d959188e5c1716828bdebf1848fb474a81c48aa
Nickson K Rop <nickyrutto@gmail.com>
2023-02-19 18:22:56 -0800

    Print an output using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main function uses printf to give an output
+ * returns (0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+}
