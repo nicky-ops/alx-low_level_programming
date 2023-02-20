0cf629b5efffc88696fd8fa5dd933300f57580e3
Nickson K Rop <nickyrutto@gmail.com>
2023-02-20 03:59:06 -0800

    fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
-  * main - Type int. Assigns a random number to variable n and prints the output
-  * Return : the main function returns (0) always
-  */
-
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ * Return: Always 0.
+*/
 int main(void)
 {
 	int n;
