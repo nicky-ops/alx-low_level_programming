942b28a2ddd9ac9214392601eb38b0d199d52f3e
Nickson K Rop <nickyrutto@gmail.com>
2023-02-19 21:34:47 -0800

    Fix the comments

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <unistd.h>
 
-/*
- * main - Entry point
- * Return: Always 0 (Success)
- * int main (void) this execute the strings
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
  */
 
 int main(void)
