be7ec06fe4879d2eccca20290f9161afdebd760a
Nickson K Rop <nickyrutto@gmail.com>
2023-02-23 10:50:53 -0800

    Program prints _putchar followed by a newline

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf('_');
+	printf('p');
+	printf('u');
+	printf('t');
+	printf('c');
+	printf('h');
+	printf('a');
+	printf('r');
+	printf('\n');
+	return (0);
+}
