1142f9e6b8512f67a6548711876bd4164b1f4bf7
Nickson K Rop <nickyrutto@gmail.com>
2023-03-08 09:21:31 -0800

    print a string recursively

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
-  * _puts_recursion - function prints a string followed by a new number
-  * @c - character input 
-  * return: void
-  */
-
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
+ */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-	{
-		_putchar('\n');
-	}
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
 }
