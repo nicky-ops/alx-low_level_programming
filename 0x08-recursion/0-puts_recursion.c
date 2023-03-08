fc878dfca52ddaf2573b44e19d20eef74f930152
Nickson K Rop <nickyrutto@gmail.com>
2023-03-08 08:43:05 -0800

    print string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#incluede 'main.h'
+
+/**
+  * _puts_recursion - function prints a string followed by a new number
+  * @c - character input 
+  * return: void
+  */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
