c67e19d740c18465a45f047f2c17e270e85a7a07
Nickson K Rop <nickyrutto@gmail.com>
2023-03-11 07:35:19 -0800

    files

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,15 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }
