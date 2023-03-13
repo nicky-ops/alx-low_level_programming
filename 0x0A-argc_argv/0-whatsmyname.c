9872d91d543ce6e8dfa2d436854eafa907e36474
Nickson K Rop <nickyrutto@gmail.com>
2023-03-13 12:07:05 -0700

    print name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - prints name of the program
+  * @argc: integer to store number of arguments
+  * @argv: array of char storing input parameters
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
