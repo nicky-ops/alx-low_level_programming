9535ba2c84ea93c57cdfc10233b6ca0cc6f5dad6
Nickson K Rop <nickyrutto@gmail.com>
2023-03-21 12:41:25 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+  * struct dog - dog information
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: owner of the dog
+  */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+#endif
