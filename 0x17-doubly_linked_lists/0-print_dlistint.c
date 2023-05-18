2ad657031d9fa03a8d6951c2a10d0c8fff27b0e3
Nickson K Rop <nickyrutto@gmail.com>
2023-05-18 03:39:58 -0700

    print all the elements of a doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,13 +9,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
-	const dlistint_t* current = h;
+	const dlistint_t *current = h;
 
 	while (current != NULL)
 	{
 		count++;
+		printf("%d\n", current->n);
 		current = current->next;
 	}
-	printf("%d\n", h->n);
 	return (count);
 }
